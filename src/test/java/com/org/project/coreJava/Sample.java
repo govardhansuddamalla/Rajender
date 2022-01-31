ba3ede6cfbc2a8346eb9a168c635fdee6899905d
Rajender koyyeda <rkoyyeda@otalio.com>
2022-01-31 21:14:01 +0530

    tasks added

---
+++ src/test/java/com/org/project/coreJava/Sample.java
@@ -0,0 +1,142 @@
+package com.org.project.coreJava;
+
+import org.openqa.selenium.By;
+
+/**
+ * Created by rajender.koyyeda on 28-01-2022.
+ */
+public class Sample {
+    public static void main(String[] agrs) {
+        System.out.println("enter your age");
+
+
+        /*Scanner sc= new Scanner(System.in);
+        String age=sc.nextLine();
+        System.out.println(" your age is: " +age);*/
+
+        /*int x = 10;
+        float y = x;
+
+        float a=10;
+
+        int b= (int) a;
+
+convertion:
+
+        WebDriver driver= new ChromeDriver();
+
+        JavascriptException js= new JavascriptException(driver);
+
+casting:
+        Scanner sc= new Scanner(System.in);
+        int x= Integer.parseInt(sc.nextLine());
+
+java generics:
+        List<WebElement> list=driver.findElements(By.xpath(""));  //multiple
+
+        List<WebElement> list=driver.findElement(By.xpath("")); //single ele
+
+
+        <T>===type
+
+                public class test(){
+
+            <T> void display(T values){
+                s.o.p(values);
+            }
+            p.s.v.m(string argd[]){
+                test object= new test();
+                object.display(2);
+                        object.display("test");
+                        object.display(2.1);
+                    }
+                }
+                */
+
+        // Operators:arthematic/comparision/logical/ternery
+
+        /*int x=111;
+        int y=11;
+        System.out.println(x);
+        System.out.println(y);
+
+String a="raj";
+String b="koyyeda";
+        System.out.println(a+b +" is bad boy ");
+
+        int z=x<y?x:y;
+
+        System.out.println(z);
+
+
+        if(x<y){
+            z=x;
+        }else if(x==y){
+            z=y;
+        }else if(x!=y){
+
+        }else{
+
+        }
+
+        if(govardha==man){
+            if(govardhan==student ){
+                if(govardhan.location==vija){
+
+                }
+            }
+        }
+
+        String x = driver.findElemet(By.xpath("")).getAttribute("color");
+        switch (x) {
+            case "red":
+                s.o.p("failed");
+
+                break;
+            case "green":
+                s.o.p("pass");
+                break;
+            case "yellow":
+                s.o.p("inprogress");
+                break;
+            default:
+                s.o.p("no color matched");
+        }
+        */
+      //Loops
+
+        /*you are in 0 with aged: 0 total age is 5
+        you are in 1 with aged: 1 total age is 5
+        you are in 2 with aged: 3 total age is 5
+        you are in 3 with aged: 6 total age is 5
+        you are in 4 with aged: 10 total age is 5
+        you are in 5 with aged: 15 total age is 5*/
+
+
+
+       /*int aged=0;
+      for(int age=5;age>=1;age--){
+          aged=0;
+          aged=aged+age;
+          System.out.println("you are in "+age+" with aged: "+aged +" total age is "+age1);
+
+      }*/
+        /*while(age1==3){
+            System.out.println("you are loop");
+            age1++;
+        }System.out.println("you are not loop");*/
+
+        /*int age1=4;
+
+        do {
+            System.out.println("you are in do " +age1);
+            age1++;
+        }
+        while(age1<6);
+            System.out.println("you are loop");*/
+
+
+
+
+    }
+}
--- src/test/resources/Tasks/task.feature
+++ src/test/resources/Tasks/task.feature
@@ -1,11 +1,19 @@
 
-  28th jan 2022
+  #     28th jan 2022
 
   #1 product name================================completed
   #2 print all products names and prices
   #3 order all products
   #4 login user name robot methods enter==============completed
   #5give username as runtime
+    #6 scanner class
 
-    31st jan 2022
-
+#           31st jan 2022
+# 7      while loop===fibbonaci series ====>0,0,1,1,2,3,5......
+# 8            reverse given number ===123==>321
+#  9     for loop===GIven number prime or not
+# 10      if -else===>given number even or odd
+# 11      switch ===>age=1-10==>kid
+#                      11-20==>teenager
+#                      21-50==>middle ager
+#                      51-100==>oldaged
